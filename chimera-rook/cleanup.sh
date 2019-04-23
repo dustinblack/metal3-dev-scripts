04806cbf5b33a252aa220e8eae26b695126a809c
Dustin Black <dblack@redhat.com>
2019-04-23 16:39:42 -0400

    add logic for attaching a block device to the VMs

--- chimera-rook/cleanup.sh
+++ chimera-rook/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-INVENTORY=$(virsh net-dhcp-leases baremetal |grep 192 | awk '{print $5}' | awk -F/ '{print $1}')
-DISK="/dev/nvme0n1"
+INVENTORY=$(virsh net-dhcp-leases baremetal | grep node | grep 192 | awk '{print $5}' | awk -F/ '{print $1}')
+DISK="/dev/vda"
 COMMANDS="sudo rm -rf /var/lib/rook; sudo sgdisk --zap-all $DISK; ls /dev/mapper/ceph-* | sudo xargs -I% -- dmsetup remove %; sudo rm -rf /dev/ceph-*"
 for i in $INVENTORY; do
   echo "Cleaning up $i..."

0c3a4149acce45f63d5f7952e085e7cf791dc011
Marko Karg <marko@redhat.com>
2019-04-29 04:53:59 -0400

    changes to helper script

--- manual.sh
+++ manual.sh
@@ -13,11 +13,12 @@ IRONIC_DATA_DIR=/opt/dev-scripts/ironic
 mkdir -p $IRONIC_DATA_DIR/html/images
 podman pod create -n ironic-pod
 PODRUNCMD="podman run -d --net host --privileged -v ${IRONIC_DATA_DIR}:/shared --pod ironic-pod"
-$PODRUNCMD --name dnsmasq --entrypoint /bin/rundnsmasq metalkube-ironic
-$PODRUNCMD --name httpd --entrypoint /bin/runhttpd metalkube-ironic
-$PODRUNCMD --name mariadb --entrypoint /bin/runmariadb --env MARIADB_PASSWORD=redhat metalkube-ironic
-$PODRUNCMD --name ironic --env MARIADB_PASSWORD=redhat metalkube-ironic
-$PODRUNCMD --name ironic-inspector metalkube-ironic-inspector
+$PODRUNCMD --name dnsmasq --entrypoint /bin/rundnsmasq quay.io/dustinblack/metalkube-ironic
+$PODRUNCMD --name httpd --entrypoint /bin/runhttpd quay.io/dustinblack/metalkube-ironic
+$PODRUNCMD --name mariadb --entrypoint /bin/runmariadb --env MARIADB_PASSWORD=redhat quay.io/dustinblack/metalkube-ironic
+$PODRUNCMD --name ironic --env MARIADB_PASSWORD=redhat quay.io/dustinblack/metalkube-ironic
+$PODRUNCMD --name ironic-inspector quay.io/dustinblack/metalkube-ironic-inspector
+
 
 # create the cluster
 sleep 30

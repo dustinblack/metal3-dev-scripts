75ba716277a09089b603a3a2f37d029e5a56470a
Marko Karg <marko@redhat.com>
2019-04-29 10:15:43 -0400

    minor fix

--- attach-disk.sh
+++ attach-disk.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo Deleting old qcow2 images
+rm -rf /home/osd*.qcow2
+
 for i in 0 1 2
 do
-  virsh attach-disk openshift_node_$i /home/osd$i.qcow2 vda --driver qemu --subdriver qcow2
+  echo Creating new qcow2 images
+  qemu-img create -f qcow2 /home/osd$i.qcow2 50G
+  echo Attaching disk to VM
+  virsh attach-disk openshift_node_$i /home/osd$i.qcow2 vda --driver qemu --subdriver qcow2 --persistent --config  --live
 done
   

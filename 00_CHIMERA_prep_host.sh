4deacf527dd8daf5950c6ea86cedcc41e265de6d
Dustin Black <dblack@redhat.com>
2019-04-30 15:36:56 -0400

    add nginx to chimera prep

--- 00_CHIMERA_prep_host.sh
+++ 00_CHIMERA_prep_host.sh
@@ -3,3 +3,11 @@
 yum -y groupinstall 'Virtualization Host'
 systemctl enable libvirtd; systemctl start libvirtd
 yum -y install git vim-enhanced
+
+yum -y install nginx
+mkdir -p /home/html
+echo 'demo' > /home/html/index.html
+sed -i s/80\ default_server/88\ default_server/g /etc/nginx/nginx.conf
+sed -i s/\/usr\/share\/nginx\/html/\/home\/html/g /etc/nginx/nginx.conf
+systemctl enable nginx
+systemctl start nginx

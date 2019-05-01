d384533b00b6573ff71db4c36b386b33c141c615
Dustin Black <dblack@redhat.com>
2019-05-01 15:45:32 -0400

    fix html path for nginx

--- 00_CHIMERA_prep_host.sh
+++ 00_CHIMERA_prep_host.sh
@@ -6,8 +6,9 @@ systemctl enable libvirtd; systemctl start libvirtd
 yum -y install git vim-enhanced
 
 yum -y install nginx
-mkdir -p /home/html
-echo 'demo' > /home/html/index.html
+WEBHOME=/opt/dev-scripts/html
+mkdir -p $WEBHOME
+echo 'demo' > ${WEBHOME}index.html
 sed -i s/80\ default_server/88\ default_server/g /etc/nginx/nginx.conf
 sed -i s/\/usr\/share\/nginx\/html/\/opt\/dev-scripts\/html/g /etc/nginx/nginx.conf
 systemctl enable nginx
